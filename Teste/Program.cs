ae854670ec6b79c415d1aedb87ebe0da30f558d5
Christophermrcf <christophermrcf@gmail.com>
2025-01-20 02:06:44 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+
+public struct Pessoa
+{
+    public string CPF;
+    public string Nome;
+    public string Telefone;
+
+    public Pessoa(string cpf, string nome, string telefone) {
+        CPF = cpf;
+        Nome = nome;
+        Telefone = telefone;
+    }
+}
+public class Abordagem
+{
+    private static List<Pessoa> ListaPessoas = new List<Pessoa>();
+    public static void Opcoes() {
+        Console.WriteLine("Escolha a opcao desejada:");
+        Console.WriteLine("1 - Cadatrar");
+        Console.WriteLine("2 - Gerar Lista");
+        Console.WriteLine("3 - Consultar ");
+        Console.WriteLine("4 - Sair");
+
+        Console.Write("Digite o número da opção desejada: ");
+        string entrada = Console.ReadLine();
+
+        switch (entrada) {
+            case "1":
+                Cadastrar();
+                break;
+            case "2":
+                GerarLista();
+                break;
+            case "3":
+                Consultar();
+                break;
+            case "4":
+                return;
+            default:
+                Console.WriteLine("Opção inválida");
+                break;
+
+        }
+    }
+
+        private static void Cadastrar() {
+        Console.WriteLine("Opção selecionada: Cadastrar");
+
+        Console.Write("Digite o CPF:");
+        string cpf = Console.ReadLine();
+
+        Console.WriteLine("Digite o seu Nome:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite o seu Telefone:");
+        string telefone = Console.ReadLine();
+
+        Pessoa NovaPessoa = new Pessoa(cpf, nome, telefone);
+        ListaPessoas.Add(NovaPessoa);
+
+        Console.WriteLine("Cadastro realizado com sucesso!");
+
+
+    }
+    private static void GerarLista() { }
+    private static void Consultar() { }
+
+    }
